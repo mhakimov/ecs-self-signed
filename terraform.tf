60748e9db2b2231147098f2feb21934b4a54d854
mhakimov <xakim@inbox.lv>
2023-11-04 23:44:11 +0200

    1st commit

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=5.13.1"
+    }
+  }
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+       bucket = "ecs-self-signed-remote-state"
+       key    = "`terraform.tfstate"
+       region = "eu-west-2"
+   }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
