eed96ec77771514eb79bf2ab8d0bbdc68d6686cd
mhakimov <xakim@inbox.lv>
2023-11-07 01:20:39 +0200

    added task definition

--- main.tf
+++ main.tf
@@ -18,45 +18,37 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_ecs_task_definition" "my_task" {
-  family                   = "my-ecs-task"
+  family                   = "sample-fargate"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = "arn:aws:iam::123456789012:role/ecsTaskExecutionRole" # Replace with your execution role ARN
 
   cpu = "256"    # CPU units
-  memory = "0.5" # GB
+  memory = "512" # GB
 
-  container_definitions = jsonencode({
-    "family": "sample-fargate", 
-    "networkMode": "awsvpc", 
-    "containerDefinitions": [
+  container_definitions = jsonencode([
         {
-            "name": "fargate-app", 
-            "image": "public.ecr.aws/docker/library/httpd:latest", 
-            "portMappings": [
+            name = "fargate-app", 
+            image = "public.ecr.aws/docker/library/httpd:latest", 
+            portMappings = [
                 {
-                    "containerPort": 80, 
-                    "hostPort": 80, 
-                    "protocol": "tcp"
+                    containerPort = 80, 
+                    hostPort = 80, 
+                    protocol = "tcp"
                 }
             ], 
-            "essential": true, 
-            "entryPoint": [
+            essential = true, 
+            entryPoint = [
                 "sh",
 		"-c"
             ], 
-            "command": [
+            command = [
                 "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
             ]
         }
-    ], 
-    "requiresCompatibilities": [
-        "FARGATE"
-    ], 
-    "cpu": "256", 
-    "memory": "512"
-})
+    ])
 }
+
+
 
 
 
