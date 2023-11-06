10b33a93af3eb38a8477a491f669cee5320263d0
mhakimov <xakim@inbox.lv>
2023-11-07 01:01:49 +0200

    added task definition

--- main.tf
+++ main.tf
@@ -16,3 +16,48 @@ resource "aws_ecs_cluster" "cluster" {
     value = "enabled"
   }
 }
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-ecs-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = "arn:aws:iam::123456789012:role/ecsTaskExecutionRole" # Replace with your execution role ARN
+
+  cpu = "256"    # CPU units
+  memory = "0.5" # GB
+
+  container_definitions = jsonencode({
+    "family": "sample-fargate", 
+    "networkMode": "awsvpc", 
+    "containerDefinitions": [
+        {
+            "name": "fargate-app", 
+            "image": "public.ecr.aws/docker/library/httpd:latest", 
+            "portMappings": [
+                {
+                    "containerPort": 80, 
+                    "hostPort": 80, 
+                    "protocol": "tcp"
+                }
+            ], 
+            "essential": true, 
+            "entryPoint": [
+                "sh",
+		"-c"
+            ], 
+            "command": [
+                "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+            ]
+        }
+    ], 
+    "requiresCompatibilities": [
+        "FARGATE"
+    ], 
+    "cpu": "256", 
+    "memory": "512"
+})
+}
+
+
+
+
