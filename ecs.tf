7b533a5311d48a1ca84c6c090c44bdae2ea1ed62
mhakimov <xakim@inbox.lv>
2023-11-20 12:54:35 +0200

    build envoy image

--- ecs.tf
+++ ecs.tf
@@ -7,14 +7,14 @@ resource "aws_ecr_repository" "ecr_repo" {
   }
 }
 
-# resource "aws_ecr_repository" "envoy_repo" {
-#   name                 = "envoy-repo"
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "envoy_repo" {
+  name                 = "envoy-repo"
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
 
 resource "aws_ecs_cluster" "cluster" {
   name = "hello-fargate"
