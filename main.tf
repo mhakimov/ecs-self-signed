271b6f279fce6c517b86427557a2aee89dcf6c80
mhakimov <xakim@inbox.lv>
2024-01-20 15:21:11 +0200

    Revert "WORKABLE: added lb and https listener."
    This reverts commit 3d87ebb9b87d95ffb2aad1401d5b6d600b25f354.

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-resource "aws_lb" "onyx" {
-  name               = "alb-onyx"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public_aza.id, aws_subnet.public_azb.id]
-}
+# resource "aws_lb" "onyx" {
+#   name               = "alb-onyx"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.lb_sg.id]
+#   subnets            = [aws_subnet.public_aza.id, aws_subnet.public_azb.id]
+# }
 
-resource "aws_lb_listener" "alb_listener_http" {
-  load_balancer_arn = aws_lb.onyx.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "alb_listener_http" {
+#   load_balancer_arn = aws_lb.onyx.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    target_group_arn = aws_lb_target_group.ecs_target_group.arn
-    type             = "forward"
-  }
-}
+#   default_action {
+#     target_group_arn = aws_lb_target_group.ecs_target_group.arn
+#     type             = "forward"
+#   }
+# }
 
 # resource "aws_lb_listener" "alb_listener_https" {
 #   load_balancer_arn = aws_lb.onyx.arn
@@ -65,14 +65,14 @@ resource "aws_lb_listener" "alb_listener_http" {
 #   validation_record_fqdns = [aws_route53_record.https_record.fqdn]
 # }
 
-resource "aws_lb_target_group" "ecs_target_group" {
-  name        = "precious-target-group"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.ecs_ss_vpc.id
-  target_type = "ip"
+# resource "aws_lb_target_group" "ecs_target_group" {
+#   name        = "precious-target-group"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.ecs_ss_vpc.id
+#   target_type = "ip"
 
-  health_check {
-    path = "/"
-  }
-}
+#   health_check {
+#     path = "/"
+#   }
+# }
