b6bb63310b2b828816acc2517ec7ec930b0ca9d7
mhakimov <xakim@inbox.lv>
2025-04-21 22:16:14 +0100

    added proxy container to task definition

--- ecs.tf
+++ ecs.tf
@@ -25,6 +25,7 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_ecs_task_definition" "my_task" {
+  task_role_arn            = aws_iam_role.ecs_execution_role.arn
   family                   = "sample-fargate"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -33,17 +34,50 @@ resource "aws_ecs_task_definition" "my_task" {
   cpu    = "256" # CPU units
   memory = "512" # GB
 
-  container_definitions = jsonencode([{
-    environment : [
-      { name = "NODE_ENV", value = "production" }
-    ],
-    essential = true,
-    image     = "962768705974.dkr.ecr.eu-west-2.amazonaws.com/fargate-repo:latest",
-    # image        = "${aws_ecr_repository.ecr_repo.repository_url}:latest",
-    name         = "fargate-app",
-    portMappings = [{ containerPort = 80 }],
-  }])
+  container_definitions = jsonencode([
+    {
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = "/ecs/ecs-self-signed",
+          awslogs-region        = "eu-west-2",
+          awslogs-stream-prefix = "ecs"
+        }
+      },
+      portMappings = [
+        {
+          hostPort      = 443,
+          protocol      = "tcp",
+          containerPort = 443
+        }
+      ],
+      cpu = 0,
+      environment = [
+        {
+          name  = "DNS_NAME",
+          value = "ecs-ss.awsblogs.info"
+        }
+      ],
+      image = "962768705974.dkr.ecr.eu-west-2.amazonaws.com/proxy:latest",
+      name  = "envoy"
+    },
+
+    {
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = "/ecs/ecs-self-signed",
+          awslogs-region        = "eu-west-2",
+          awslogs-stream-prefix = "ecs"
+        }
+      },
+      cpu   = 0,
+      image = "962768705974.dkr.ecr.eu-west-2.amazonaws.com/app:latest"
+      name  = "service"
+    }
+  ])
 }
+
 
 resource "aws_ecs_service" "bar" {
   name            = "test-service"
