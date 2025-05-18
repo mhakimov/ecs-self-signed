4e0cfceca3829685ee9822cc11871b0a9d4397bd
mhakimov <xakim@inbox.lv>
2025-05-18 12:30:09 +0100

    FIX: remove extra listener & cert

--- alb.tf
+++ alb.tf
@@ -23,25 +23,6 @@ resource "aws_lb_listener" "alb_listener_https" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
 
-  certificate_arn = data.aws_acm_certificate.webapp_cert.arn
-
-  default_action {
-    target_group_arn = aws_lb_target_group.ecs_target_group.arn
-    type             = "forward"
-  }
-}
-
-data "aws_acm_certificate" "webapp_cert" {
-  domain   = var.domain_name
-  statuses = ["ISSUED"]
-}
-
-resource "aws_lb_listener" "alb_listener_https" {
-  load_balancer_arn = aws_lb.onyx.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-
   certificate_arn = aws_acm_certificate.cert.arn
 
   default_action {
