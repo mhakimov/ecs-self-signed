85e4cd36575729feaf1019d9651f0b82d5ec89bd
mhakimov <xakim@inbox.lv>
2025-05-19 15:52:23 +0100

    FIX: change aws_lb_target_group port to 8080

--- alb.tf
+++ alb.tf
@@ -34,7 +34,7 @@ resource "aws_lb_listener" "alb_listener_https" {
 
 resource "aws_lb_target_group" "ecs_target_group" {
   name        = "precious-target-group"
-  port        = 80
+  port        = 8080
   protocol    = "HTTP"
   vpc_id      = aws_vpc.ecs_ss_vpc.id
   target_type = "ip"
